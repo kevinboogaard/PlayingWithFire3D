8b0caf806519fd702b084ca8933a4212e4375b89
kevinboogaard <kevin.boogaard3@gmail.com>
2015-09-07 15:09:47 +0200

    First push
    the basics

---
+++ Assets/_scripts/_components/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void MoveX(int dir ,float movementspeed)
+    {
+        transform.Translate(new Vector3(dir, 0, 0) * movementspeed * Time.deltaTime);
+    }
+
+    public void MoveZ(int dir ,float movementspeed)
+    {
+        transform.Translate(new Vector3(0, 0, dir) * movementspeed * Time.deltaTime);
+    }
+}
---
+++ Assets/_scripts/_components/PlayerBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Movement))]
+public class PlayerBehaviour : MonoBehaviour {
+
+    private Movement movementcomp;
+    private int _movementspeed = 10;
+	// Use this for initialization
+	void Start () {
+        movementcomp = GetComponent<Movement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            movementcomp.MoveX(-1, _movementspeed);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            movementcomp.MoveZ(1, _movementspeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            movementcomp.MoveX(1, _movementspeed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            movementcomp.MoveZ(-1, _movementspeed);
+        }
+
+	}
+}
